c8f62d218425cb7e7feda58cd4a21d7b10b70b56
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-05-31 20:36:48 +0530

    data type notes added

---
+++ 2_datatypes.js
@@ -0,0 +1,29 @@
+console.group("This is a message to know the Data Type of the variable");
+
+let age = 25;
+let temperature = -5.6;
+
+console.log(typeof(age));
+
+
+console.group("This is a message to know the Data Type of the variable");
+
+const Designation = "Software Test engineer";
+
+console.log(typeof(Designation));
+
+
+console.group("This is a message to know the Data Type of the variable");
+
+let person = {
+    name: "Alice",
+    age: 30,
+    isEmployed: true
+};
+
+console.log(typeof(person));
+
+" output: object"
+
+"In JavaScript, an object is a collection of key-value pairs where the keys are strings (or symbols) and the values can be of any data type"
+"including other objects. Objects are used to store related data and functionality together."
