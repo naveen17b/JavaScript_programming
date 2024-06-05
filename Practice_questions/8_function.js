c9d7b7776c287d20ad50470ac696ce73e465fd9b
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-06-05 23:53:22 +0530

    new function notes added

---
+++ Practice_questions/8_function.js
@@ -0,0 +1,11 @@
+//declaring function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+
+//function expression can be stored in variable:
+
+const greet = function(name) {
+    return `Hello, ${name}!`;
+};
