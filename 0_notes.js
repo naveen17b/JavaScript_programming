4fb7b7dd1faf7560d9214251d0a6fc50e688c88f
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-07-03 09:10:58 +0530

    added new file notes and add the some text in the data types file

---
+++ 0_notes.js
@@ -0,0 +1,10 @@
+/**
+ * Type conversions: When an operator is applied to the “wrong” type of value, JavaScript will
+    quietly convert that value to the type it needs, using a set of rules that often
+    aren’t what you want or expect. This is called type coercion.
+ */
+
+
+/**
+ * 
+ */
