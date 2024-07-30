70be2b0d0200b3ab8aebba078df21dc79d76d4fa
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-07-30 23:26:39 +0530

    inheritance file is added to the notes

---
+++ 11_Inheritance.js
@@ -0,0 +1,55 @@
+/**
+ * Inheritance:
+ */
+
+
+class Vehicle {
+
+    constructor(AutomobileBrand, color, engine, model) {
+
+        this.AutomobileBrand = AutomobileBrand;
+        this.engine = engine;
+        this.color = color;
+        this.model = model;
+    }
+
+    company() {
+        console.log('This is a ' + this.AutomobileBrand);
+    }
+
+    colorOfAutomobile() {
+        console.log("which is " + this.color);
+
+    }
+
+    engineOfAutomobile() {
+        console.log("the engine of the vehicle" + this.engine);
+
+    }
+
+    model() {
+        console.log('this is ' + this.model);
+
+    }
+}
+
+
+//Defining the child class
+//super word in the constructor is calling the constructor from the parent
+
+class Motorcycle extends Vehicle {
+    constructor(color, engine, model, fuelEffeciency) {
+        super(color, engine, model);
+        this.fuelEffeciency = fuelEffeciency;
+    }
+    doWheelie() {
+        console.log("Driving on one wheel!");
+    }
+}
+
+let motor = new Motorcycle(0, "Black", 250, 28.5);
+console.log(motor.color);
+
+motor.doWheelie();
+
+
