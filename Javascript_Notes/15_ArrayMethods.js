076107b1e77b967a319040188cf334b9b5a834af
Bammidi Naveen Krishna <bnaveen1794@gmail.com>
2024-08-16 22:44:01 +0530

    minor changes in array methods added

--- Javascript_Notes/15_ArrayMethods.js
+++ Javascript_Notes/15_ArrayMethods.js
@@ -10,6 +10,8 @@ function printArrayElements(element, index) {
 
 }
 
+//forEach(): Performs the specified action for each element in an array.
+
 array_items.forEach(printArrayElements);
 
 
@@ -17,16 +19,20 @@ array_items.forEach(printArrayElements);
  * Filtering array:
  */
 
-function filterArray(element, index) {
+function filterArray(element) {
 
     //console.log('the element : ' + element + ' is at the index position : ' + index);
     return typeof element === 'string';
 
 }
 
+//Filter: Returns the elements of an array that meet the condition specified in a callback function.
+
 let filteredArrayElement = array_items.filter(filterArray);
 
 console.log(filteredArrayElement);
 
+//Every: Determines whether all the members of an array satisfy the specified test.
 
 console.log(array_items.every(filterArray));
+
