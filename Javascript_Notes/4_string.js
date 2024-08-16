67e42021ba67f00d82ce35b4e0029abb02aca9d1
Naveen krishna <naveen1794@gmail.com>
2024-08-16 11:18:21 +0530

    Created loop folder and added new file in it and merged the functions and string into other functions and string file duplicates

--- Javascript_Notes/4_string.js
+++ Javascript_Notes/4_string.js
@@ -35,3 +35,32 @@ const splitStringValue = "Naveen%20krishna";
 
 console.log(splitStringValue.split());
 console.log(splitStringValue.replace('%20'," "));
+
+let num1 = 19
+
+let stringValueNew = String (num1)
+
+console.log(stringValue)
+
+console.log(typeof stringValueNew)
+
+let name1 = "Naveen"
+
+let name2 = " Krishna"
+
+let name3 = name1 + name2
+
+console.log(name3)
+
+
+console.time(stringValueNew)
+
+let n1 = "1" + 2
+
+let n2 = 2 + "56"
+
+console.timeLog(stringValueNew)
+
+console.log(n1)
+
+console.log(n2)
