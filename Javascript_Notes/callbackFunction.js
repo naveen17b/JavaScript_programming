c4c0c1e3cb761d41f1ebbf88dfeff617888833ac
Naveen Krishna <bnaveen1794@gmail.com>
2025-01-10 15:11:48 +0530

    call back function with a example and the sync in javascript concepts were added to the notes

--- Javascript_Notes/callbackFunction.js
+++ Javascript_Notes/callbackFunction.js
@@ -1,1 +1,40 @@
- 
+/**
+ * CallBack Function:
+ * A callback function in JavaScript is a function that is passed as an argument to another function, 
+ * and it is executed (or "called back") after some operation has been completed. 
+ * Callbacks are a way to ensure that a particular code runs only after a specific task has finished, 
+ * making them especially useful for asynchronous programming.
+ */
+
+// Example 1:
+// Define a function that takes a callback function as an argument
+function greet(name, callback) {
+    console.log('Hello', name);
+    callback();
+}
+
+function sayWelcome() {
+    console.log("Welcome to the world of callbacks");
+}
+
+// Call greet and pass sayGoodbye as a callback
+greet("krishna", sayWelcome);
+
+
+
+// Example 2:
+
+function greet(name, callback) {
+    setTimeout(() => {
+        console.log("Hello", name);
+        callback('this is a async callback');
+    }, 5000);
+}
+
+function sayWelcome(msg) {
+    console.log(msg);
+}
+
+// Call greet and pass sayGoodbye as a callback
+greet("Programmer", sayWelcome);
+
